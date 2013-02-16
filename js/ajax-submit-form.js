e77bf694d5988f35c68191d545535437945afef1
wp_candyman <wp_candyman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-16 10:06:12 +0000

    version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/zoho-crm-integrator/trunk@668852 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/ajax-submit-form.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-        // wait for the DOM to be loaded 
-        jQuery(document).ready(function() { 
-            // bind 'myForm' and provide a simple callback function 
-            jQuery('#lead_form').ajaxForm(function() { 
-                alert("Thank you! Your contact was sucessfully added"); 
-            }); 
-        }); 
